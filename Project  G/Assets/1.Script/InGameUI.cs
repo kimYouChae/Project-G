0a3e325685340ae5538498891b2cca07ff036d38
김지현 <kjhstar2486@naver.com>
2025-07-29 16:28:32 +0900

    ✨ Feat: 게임UI 추가 , 충돌 시 애니메이션 추가

---
+++ Project  G/Assets/1.Script/InGameUI.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;  // 두트윈 
+
+public class InGameUI : MonoBehaviour
+{
+    private static InGameUI instance;   // 인스턴스
+
+    [SerializeField] Camera camera;
+    [SerializeField] float cameraFarZ = -5f;
+
+    [Header("===HighLight===")]
+    [SerializeField] GameObject highlightBG;    // 하이라이트 이미지 
+    const float highlightMinScale = 1f;
+    const float highlightTime = 1f;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(instance);
+        }
+
+        instance = this;
+    }
+
+    public static InGameUI GetInstance()
+    {
+        if (instance == null)
+        {
+            Debug.LogError("InGameUI 인스턴스가 존재하지 않습니다.");
+            return null;
+        }
+
+        return instance;
+    }
+
+    public void HighlightPlayer(Transform trs) 
+    {
+        // 카메라 위치 이동 
+        camera.transform.position = new Vector3(trs.position.x, trs.position.y, cameraFarZ);
+
+        if(highlightBG.activeSelf == false)
+            highlightBG.SetActive(true);
+
+        // 크기를 min까지 줄이는 애니메이션 실행 
+        RedueceAnimation();
+    }
+
+    private void RedueceAnimation() 
+    {
+        highlightBG.GetComponent<RectTransform>().DOScale(
+            new Vector3(highlightMinScale, highlightMinScale, 0) ,
+            highlightTime)
+            .SetUpdate(true);   // timel.scale에 영향 받지 않는 
+    }
+}
