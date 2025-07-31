2f5444a35eae19d1e6444e07c55672d6e506cc8c
김지현 <kjhstar2486@naver.com>
2025-07-31 12:22:33 +0900

    ⚡️ Add: 버튼 이미지 추가 (픽셀변경)

---
+++ Project  G/Assets/1.Script/UI/ButtonClickImage.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonClickImage : MonoBehaviour
+{
+    [Header("===이미지 세팅===")]
+    [SerializeField] Sprite buttonClickSprite;
+
+    [Space]
+    [SerializeField] Image myImage;
+    [SerializeField] Sprite oriSprite;
+    [SerializeField] EventTrigger trigger;
+
+    private bool isClicked = false;
+
+    private void Start()
+    {
+        myImage = GetComponent<Image>();
+        oriSprite = GetComponent<Image>().sprite;
+        trigger = GetComponent<EventTrigger>();
+
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerEnter;
+        entry.callback.AddListener((data) => { OnHover(); }); // delegate 또는 람다로 처리
+
+        trigger.triggers.Add(entry);
+
+        EventTrigger.Entry entry2 = new EventTrigger.Entry();
+        entry2.eventID = EventTriggerType.PointerExit;
+        entry2.callback.AddListener((data) => { OutHover(); });
+
+        trigger.triggers.Add(entry2);
+    }
+
+    private void OnHover()
+    {
+        myImage.sprite = buttonClickSprite;
+    }
+
+    private void OutHover() 
+    {
+        myImage.sprite = oriSprite;
+    }
+
+}
