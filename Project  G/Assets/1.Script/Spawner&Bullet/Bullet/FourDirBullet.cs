786db906b298d1a1ee47e83334c3f3fea3ced6ed
김지현 <kjhstar2486@naver.com>
2025-09-06 00:34:11 +0900

    ✨ Feat: 총알 스프라이트 추가 , FourDIr 슬라임 추가, 애니메이션 추가

--- Project  G/Assets/1.Script/Spawner&Bullet/Bullet/FourDirBullet.cs
+++ Project  G/Assets/1.Script/Spawner&Bullet/Bullet/FourDirBullet.cs
@@ -9,14 +9,21 @@ public class FourDirBullet : MonoBehaviour
 {
     [SerializeField] PhotonView view;
     [SerializeField] GameObject basicBullet;
-
+    
     private void Start()
     {
         view = gameObject.GetComponent<PhotonView>();
 
+        StartCoroutine(BulletShoot());
+    }
+
+    IEnumerator BulletShoot() 
+    {
+        yield return new WaitForSeconds(3f);
+
         // 4방향으로 총알 발사 
         // 총알 발사는 RPC 여야함 -> Photonview있어야함 -> 네트워크 객체여야함
-        if (view.IsMine) 
+        if (view.IsMine)
         {
             view.RPC(nameof(RPC_ShootBasciBullet), RpcTarget.AllBuffered, Vector2.up);
             view.RPC(nameof(RPC_ShootBasciBullet), RpcTarget.AllBuffered, Vector2.right);
