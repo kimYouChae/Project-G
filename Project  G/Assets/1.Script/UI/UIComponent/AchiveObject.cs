b6110e32dd1623c78a67efb2030f64cb308cd278
김지현 <kjhstar2486@naver.com>
2025-10-16 18:13:17 +0900

    👷 Chore: 도전과제 오브젝트/스크립트 추가

---
+++ Project  G/Assets/1.Script/UI/UIComponent/AchiveObject.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchiveObject : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI achiveTitleText;
+    [SerializeField] private TextMeshProUGUI progressText;
+    [SerializeField] private Image completeImage;   // 성공 여부 이미지 
+
+    public void SetAchiveTitle(string str) {  achiveTitleText.text = str; }
+
+    public void SetProgressText(string str) {  progressText.text = str; }
+
+    public void OnOffCompleteImage(bool flag) {  completeImage.gameObject.SetActive(flag); }
+}
