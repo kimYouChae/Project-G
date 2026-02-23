0021e458e7ba228f10b5e4148f405e9fdb60814b
김지현 <kjhstar2486@naver.com>
2026-02-23 17:38:15 +0900

    👷 Chore: GameDataModel 이름 변경

--- Project  G/Assets/1.Script/0.GameServices/GameDataModel.cs
+++ Project  G/Assets/1.Script/0.GameServices/GameDataModel.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GamdDataModel : IGameDataModel
+public class GameDataModel : IGameDataModel
 {
     // 게임데이터 API 가 끝나면 여기에 정보 담김
     private BestScoreUpdateResponse bsResponse;
