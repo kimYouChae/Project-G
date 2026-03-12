5b20a26b01dc791fb7c9ecc22217f18f5d35bc00
김지현 <kjhstar2486@naver.com>
2026-03-12 18:50:18 +0900

    ✨ Feat: SFX 재생 정책 클래스 생성

--- Project  G/Assets/1.Script/Sound/SFXManager.cs
+++ Project  G/Assets/1.Script/Sound/SFXManager.cs
@@ -2,17 +2,37 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SFXManager : MonoBehaviour
 {
     private const float SFX_RETURN_DELAY = 0.15f;
 
-    [Header("===Container===")]
-    protected Dictionary<SFXType, AudioSource> typeBySource;
-    protected Dictionary<SFXType, AudioClip> typeByClip;
+    [Header("===Transform===")]
+    private Transform sfxTrs;       // 오디오 소스 담아둘 trs
 
-    protected async Task InitAudioClip()
+    [Header("===Container===")]
+    private Dictionary<SFXType, AudioClip> typeByClip;      // 타입별 오디오 클립 
+    private Dictionary<SFXType, AudioSource> typeBySource;  // 타입별 오디오 소스 
+    private AudioSource commmonAudioSource;     // 공용 오디오소스 
+
+    [Header("===SFX Policy===")]
+    private SFXPolicy sfxPolicy;    // sfx 타입별 실행정책
+
+    private void Awake()
+    {
+        // 정책 생성 
+        sfxPolicy = new SFXPolicy();
+
+        // 오디오 클립 가져오기
+        InitAudioClip();
+
+        // 오디오 소스 세팅 
+        InitAudioSource();
+    }
+
+    private void InitAudioClip()
     {
         try 
         {
@@ -34,8 +54,50 @@ public class SFXManager : MonoBehaviour
 
     }
 
+    private void InitAudioSource() 
+    {
+        // SFX 오브젝트 하나 만들기 (여기에 오디오소스 추가예정)
+        sfxTrs = SoundManager.Instance.InstanceSoundObject(nameof(SFXManager));
+
+        // 정책따라 오디오소스 생성
+        int length = Extension.EnumCount<SFXType>();
+        for(int i = 0; i < length; i++) 
+        {
+            SFXType sType = (SFXType)i;
+            PlayPolicy policy = sfxPolicy.GetPolicy(sType);
+
+            // 개인 오디오소스가 필요하면
+            if (policy.isIndividualSoundSource)
+            {
+                // 오디오소스 생성후 딕셔너리에 넣기
+                AudioSource indiSource = sfxTrs.AddComponent<AudioSource>();
+                typeBySource.Add(sType, indiSource);
+
+                // 오디오 믹서 세팅 
+                SettingAudioMixer(indiSource);
+                continue;
+            }
+
+            // 공용 오디오소스이면 
+            if (commmonAudioSource != null)
+                continue;
+
+            AudioSource source = sfxTrs.AddComponent<AudioSource>();
+            commmonAudioSource = source;
+            SettingAudioMixer(source);
+        }
+    }
+
+    private void SettingAudioMixer(AudioSource source) 
+    {
+        // 믹서 세팅 
+
+    }
+
     public void PlaySFX(SFXType type) 
     {
+        //##TODO : 실행할 때 타입에 맞는 정책을 바탕으로 실행해야됨 
+
         // 1. 타입에 해당하는 오디오소스 받기
         AudioSource source = GetAudioSource(type);
         if (source == null)
@@ -60,7 +122,7 @@ public class SFXManager : MonoBehaviour
     }
 
     // type에 해당하는 오디오소스 return
-    protected AudioSource GetAudioSource(SFXType type)
+    private AudioSource GetAudioSource(SFXType type)
     {
         if (typeBySource.ContainsKey(type))
             return typeBySource[type];
---
+++ Project  G/Assets/1.Script/Sound/SFXPolicy.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class PlayPolicy 
+{
+    // 개인 Sound Source 를 사용하는지 
+    // or 공통 Sound Source 사용 ( play on shot 방식 )
+    public bool isIndividualSoundSource;
+
+    // 동기화 되는지
+    // or 로컬에서만 실행되는지 ( 비동기 )
+    public bool isSync;
+
+    public PlayPolicy(bool isindivi, bool isSyn)
+    {
+        this.isIndividualSoundSource = isindivi;
+        this.isSync = isSyn;
+    }
+}
+
+public class SFXPolicy 
+{
+    /// <summary>
+    /// SFX 규칙에 대해서 정리
+    /// 1. 개인 soundSource를 사용하는지 
+    /// 2. 동기화 되는지
+    /// </summary>
+    
+    private Dictionary<SFXType, PlayPolicy> sfxPolicyDic;
+
+    public SFXPolicy() 
+    {
+        sfxPolicyDic = new Dictionary<SFXType, PlayPolicy>();
+
+        // UI
+        sfxPolicyDic.Add(SFXType.UIClick, new PlayPolicy(false, false));
+        sfxPolicyDic.Add(SFXType.UIBack, new PlayPolicy(false, false));
+        sfxPolicyDic.Add(SFXType.UIPopup, new PlayPolicy(false, false));
+
+        // InGame
+        sfxPolicyDic.Add(SFXType.Countdown, new PlayPolicy(false, true));
+        sfxPolicyDic.Add(SFXType.GameStart, new PlayPolicy(false, true));
+
+        // Character
+        sfxPolicyDic.Add(SFXType.CharacterFootstep, new PlayPolicy(true, false));
+        sfxPolicyDic.Add(SFXType.CharacterDeath, new PlayPolicy(false, true));
+        sfxPolicyDic.Add(SFXType.ShieldBlock, new PlayPolicy(false, false));
+        sfxPolicyDic.Add(SFXType.ScoreCoin, new PlayPolicy(false, false));
+        sfxPolicyDic.Add(SFXType.InvincibleSkill, new PlayPolicy(false, false));
+
+        // Spawner
+        sfxPolicyDic.Add(SFXType.BulletSpawnerShot, new PlayPolicy(false, false));
+        sfxPolicyDic.Add(SFXType.MissileSpawnerShot, new PlayPolicy(false, false));
+        sfxPolicyDic.Add(SFXType.MissileFly, new PlayPolicy(true, false));
+        sfxPolicyDic.Add(SFXType.MissileExplosion, new PlayPolicy(false, false));
+        sfxPolicyDic.Add(SFXType.LaserCharge, new PlayPolicy(true, true));
+        sfxPolicyDic.Add(SFXType.LaserFire, new PlayPolicy(true, true));
+    }
+
+    public PlayPolicy GetPolicy(SFXType type)
+    {
+        if (sfxPolicyDic.TryGetValue(type, out PlayPolicy policy))
+        {
+            return policy;
+        }
+
+        Debug.LogWarning($"SFXPolicy not found for {type}");
+        return null;
+    }
+}
