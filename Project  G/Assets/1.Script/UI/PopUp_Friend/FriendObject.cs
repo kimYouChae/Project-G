97939a62f0ba304658610bb5aa18e42a5659b667
김지현 <kjhstar2486@naver.com>
2026-03-31 20:25:09 +0900

    ✨ Feat: 친구 UI 관련 스크립트 작성

--- Project  G/Assets/1.Script/UI/PopUp_Friend/FriendObject.cs
+++ Project  G/Assets/1.Script/UI/PopUp_Friend/FriendObject.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class FriendObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Image profileImage;
+    [SerializeField] TextMeshProUGUI nameText;
+    [SerializeField] int index;     // 몇번째 friend 오브젝트인지 
+    [SerializeField] Button friendSelectButton; // "초대" 버튼
+    [SerializeField] TextMeshProUGUI buttonText;    // 버튼의 "초대" 텍스트 
+
+    private FriendPopUP parentPopup;
+
+    private void Awake()
     {
-        
+        friendSelectButton.onClick.AddListener(SelectFriendObj);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void SetParentPopup(FriendPopUP popup) 
+    { 
+        parentPopup = popup;
     }
+
+    public void UpdateFriendObj(Texture2D texture, string name, int index) 
+    {
+        // 이미지 세팅
+        Sprite sprite = Sprite.Create(
+        texture,
+        new Rect(0, 0, texture.width, texture.height),
+        new Vector2(0.5f, 0.5f)
+        );
+
+        profileImage.sprite = sprite;
+
+        // 닉네임, 인덱스 세팅
+        nameText.text = name;
+        this.index = index;
+    }
+
+    private void SelectFriendObj()
+    {
+        parentPopup.InviteFriendByIndex(index);
+    }
+
+
 }
--- Project  G/Assets/1.Script/UI/PopUp_Friend/FriendPopUP.cs
+++ Project  G/Assets/1.Script/UI/PopUp_Friend/FriendPopUP.cs
@@ -1,11 +1,17 @@
 using Photon.Realtime;
+using Steamworks;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class FriendPopUP : UIPopUP
 {
+    [Header("FriendPopUP")]
+    [SerializeField]
+    private GameObject scrollView;
     [SerializeField]
     private Transform content;  // 친구 목록 스크롤뷰 content에 해당하는 부분 
     [SerializeField]
@@ -19,6 +25,8 @@ public class FriendPopUP : UIPopUP
 
     public void OpenFriendPopUP() 
     {
+        scrollView.SetActive(false);
+        loadingText.gameObject.SetActive(true);
         loadingText.text = "(로컬라이징 전 입니다) 친구 목록 불러오는중";
 
         // 리스트가 비어있으면 생성 
@@ -30,10 +38,10 @@ public class FriendPopUP : UIPopUP
             }
         }
 
-        StartCoroutine(temp());
+        StartCoroutine(LoadFriendsAndOpenUI());
     }
 
-    IEnumerator temp() 
+    IEnumerator LoadFriendsAndOpenUI() 
     {
         SteamScript.Instance.GetSteamFriend();
 
@@ -42,6 +50,7 @@ public class FriendPopUP : UIPopUP
 
         // 텍스트 끄기 
         loadingText.gameObject.SetActive(false);
+        scrollView.SetActive(true);
 
         // UI 생성
         MakeUi();
@@ -49,19 +58,56 @@ public class FriendPopUP : UIPopUP
 
     private void MakeUi() 
     {
-        var friendsInfo = SteamScript.Instance.FriendCSteamIDs;
+        List<CSteamID> friendsInfo = SteamScript.Instance.FriendCSteamIDs;
+
+        // 1. 부족한 만큼 오브젝트 생성
+        while(friendObjList.Count < friendsInfo.Count) 
+        {
+            // 생성 후, friendObjList에 넣음 
+            // "친구수"까지(조건 만족할 때 까지) 생성 
+            InstanceFriendObject();
+        }
 
         // 데이터 넣기 
         for (int i = 0; i < friendsInfo.Count; i++) 
         {
-            FriendObject obj = friendObjList[i];
+            SettingFriendObj(i);
+        }
+
+        // 혹시 오브젝트가 친구수 보다 많다면 끄기 
+        for (int i = friendsInfo.Count; i < friendObjList.Count; i++)
+        {
+            friendObjList[i].gameObject.SetActive(false);
+        }
+
+        void SettingFriendObj(int index) 
+        {
+            CSteamID friendData = friendsInfo[index];
+
+            // 프로필 이미지, 이름 필요함
+            Texture2D texture = SteamScript.Instance.ReturnProfileTexureByCSteamID(friendData);
+            string name = SteamScript.Instance.ReturnNickNameByCSteamID(friendData);
+
+            FriendObject obj = friendObjList[index];
+            // 꺼져있으면 켜기
+            if(obj.gameObject.activeSelf == false)
+                obj.gameObject.SetActive(true);
+            // 데이터 업데이트
+            obj.UpdateFriendObj(texture, name, index);
         }
     }
 
     private void InstanceFriendObject() 
     {
         FriendObject obj = Instantiate(friendObj);
+        obj.SetParentPopup(this);
         obj.gameObject.transform.SetParent(content, false);
         friendObjList.Add(obj);
+    }
+
+    public void InviteFriendByIndex(int index) 
+    {
+        // 친구 초대 실행 
+        SteamScript.Instance.InviteFriendByIndex(index);
     }
 }
