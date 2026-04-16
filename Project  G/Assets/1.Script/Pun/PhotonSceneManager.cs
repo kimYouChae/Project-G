b6cabff1eb00144cc77ec3c265dcd9003f640ec5
김지현 <kjhstar2486@naver.com>
2026-04-16 21:33:15 +0900

    ✨ Feat: 씬 전환 시 실행 액션실행 기능 추가

--- Project  G/Assets/1.Script/Pun/PhotonSceneManager.cs
+++ Project  G/Assets/1.Script/Pun/PhotonSceneManager.cs
@@ -1,18 +1,64 @@
 using ExitGames.Client.Photon.StructWrapping;
 using Photon.Pun;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class PhotonSceneManager : Singleton<PhotonSceneManager>
 {
+    [Header("===Scene INFO===")]
+    [SerializeField] private string nowSceneName;
+    [SerializeField] private SceneType nowSceneType;
+
+    [SerializeField]
+    private Dictionary<SceneType, Action> sceneTypeByAction;
+
     protected override void Singleton_Awake()
     {
         SetUpDontDestroy();
+
+        // 딕셔너리 초기화 
+        sceneTypeByAction = new Dictionary<SceneType, Action>();
+    }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    public void RegisterAction(SceneType sceneType, Action action) 
+    {
+        if (sceneTypeByAction.ContainsKey(sceneType))
+        {
+            sceneTypeByAction[sceneType] += action;
+        }
+        else
+        {
+            sceneTypeByAction[sceneType] = action;
+        }
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log($"씬 로드 완료: {scene.name}");
+
+        if (!sceneTypeByAction.ContainsKey(nowSceneType))
+        {
+            Debug.Log($"[PhotonSceneManager] 씬 타입에 해당하는 액션이 없습니다");
+            return;
+        }
+
+        sceneTypeByAction[nowSceneType]?.Invoke();
     }
 
     // 임시 : 씬 동기 전환 
-
     public void ChangeGameScene() 
     {
         // 현재 방 정보의 커스텀 정보에 접근 (hashTable에서 matType검사)
@@ -52,16 +98,17 @@ public class PhotonSceneManager : Singleton<PhotonSceneManager>
 
     public void ChangeScene(SceneType type) 
     {
-        string nextSceneString = Define.sceneNames[type];
+        nowSceneType = type;    
+        nowSceneName = Define.sceneNames[type];
 
-        if (nextSceneString == null)
+        if (nowSceneName == null)
             return;
 
         // 만약 Time.scaleTime이 0이라면 다시 1로 돌리기
         if (Time.timeScale == 0)
             TimeManager.Play();
 
-        PhotonNetwork.LoadLevel(nextSceneString);
+        PhotonNetwork.LoadLevel(nowSceneName);
     }
 
 }
