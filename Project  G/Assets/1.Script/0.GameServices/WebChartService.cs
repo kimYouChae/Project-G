444dc26581ccbbbfd2e489ddafe10c9911d0fba0
김지현 <kjhstar2486@naver.com>
2026-05-26 19:37:55 +0900

    ✨ Feat: 차트 버전 관리 추가

--- Project  G/Assets/1.Script/0.GameServices/WebChartService.cs
+++ Project  G/Assets/1.Script/0.GameServices/WebChartService.cs
@@ -1,10 +1,11 @@
+using Newtonsoft.Json;
 using System;
 using System.Collections;
-using UnityEngine.Networking;
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
-using Newtonsoft.Json;
-using Unity.VisualScripting;
-using UnityEditor.Build;
+using static UnityEditor.LightingExplorerTableColumn;
+
 
 public class WebChartService : IChartService
 {
@@ -16,6 +17,9 @@ public class WebChartService : IChartService
     private int serverChartVersion;     // API로 리턴되는 차트 버전
     private bool getChartFailed;         // 차트Get 할 때 성공,실패 flag
 
+    private Dictionary<DataType, string> tempChartByjson;   // json 임시 저장용
+    private string cacheDir = Path.Combine(Application.persistentDataPath, "ChartCache"); // 임시 json 로컬 저장 경로
+
     public WebChartService(string url, string versionUrl)
     {
         this.chartUrl = url;
@@ -25,6 +29,8 @@ public class WebChartService : IChartService
         // 현재 저장된 버전 없으면 -1 
         localChartVersion = PlayerPrefs.GetInt(versionKey, -1);
         getChartFailed = false;
+
+        tempChartByjson = new Dictionary<DataType, string>();
     }
 
     public IEnumerator ChartService()
@@ -40,53 +46,86 @@ public class WebChartService : IChartService
             );
 
         // 차트 버전 비교 
-        // 1. 버전이 같으면 -> pass
-        if (serverChartVersion == localChartVersion)
-        {
-            yield break;
-        }
-        // 2. 버전이 다르면 / 로컬에 저장된 chartVersion이 없으면 
-        if (serverChartVersion != localChartVersion
-            || localChartVersion == -1) 
-        {
-            // 차트 불러오기 
-            DataType[] array = (DataType[])Enum.GetValues(typeof(DataType));
-            for (int i = 0; i < array.Length; i++)
-            {
-                yield return GetChart(array[i]);
-
-                // 만약 차트 가져오기가 하나라도 실패 한다면, 
-                if(getChartFailed)
-                {
-                    break;
-                }
-            }
-
-            if (getChartFailed)
-            {
-                // 차트 fall back 실행
-                ChartFallback();
-                yield break;
-            }
-
-            // 차트를 로컬에 저장 
-
-            // 버전 갱신
-
-            yield break;
-        }
-        // 3. -1일 때 : api 실패했을 때
+        // 1. -1일 때 : api 실패했을 때
         if (serverChartVersion == -1)
         {
             // 차트 fall back 실행
             ChartFallback();
             yield break;
         }
+
+        // 2. 버전이 같으면 -> pass
+        if (serverChartVersion == localChartVersion)
+            yield break;
+        
+        // 3. 버전이 다르거나 , 로컬에 없을 때  
+        // 차트 불러오기 
+        DataType[] array = (DataType[])Enum.GetValues(typeof(DataType));
+        for (int i = 0; i < array.Length; i++)
+        {
+            yield return GetChart(array[i]);
+
+            // 만약 차트 가져오기가 하나라도 실패 한다면, 
+            if(getChartFailed)
+                break;
+        }
+
+        if (getChartFailed)
+        {
+            // 차트 fall back 실행
+            ChartFallback();
+            yield break;
+        }
+
+        // 차트를 로컬에 저장 ( 임시로 저장된 json을 저장하기)
+        for (int i = 0; i < array.Length; i++) 
+        {
+            string path = Path.Combine(cacheDir, $"{array[i]}.json");
+            Directory.CreateDirectory(cacheDir);
+            foreach (var kv in tempChartByjson)
+                File.WriteAllText(path, kv.Value);
+        }
+
+        // 버전 갱신
+        PlayerPrefs.SetInt(versionKey, serverChartVersion);
+        PlayerPrefs.Save();
+
+        yield break;
+
     }
 
     private void ChartFallback()
     {
-    
+        // 1. 로컬에 저장된 JSON파일이 있는지 
+        string cacheDir = Path.Combine(Application.persistentDataPath, "ChartCache");
+
+        // 1. 캐시 있으면 캐시 사용
+        if (Directory.Exists(cacheDir))
+        {
+            // persistentDataPath에서 읽기
+            DataType[] array = (DataType[])Enum.GetValues(typeof(DataType));
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == DataType.None) 
+                    continue;
+
+                string path = Path.Combine(cacheDir, $"{array[i]}.json");
+                if (File.Exists(path))
+                    TypeByChartHandler(array[i], File.ReadAllText(path));
+            }
+
+            return;
+        }
+
+        // 2. 캐시가 없으면 Resource 하위 json에 접근 (완전 최후의 방법)
+        TextAsset[] textAssets = ResourceManager.Instance.FallBackChartTextfile;
+
+        for (int i = 0; i < textAssets.Length; i++)
+        {
+            DataType type = Extension.StringToEnum<DataType>(textAssets[i].name);
+
+            TypeByChartHandler(type, textAssets[i].text);
+        }
     }
 
     public IEnumerator GetChart(DataType dataType)
@@ -132,6 +171,9 @@ public class WebChartService : IChartService
     private void ParsingChart(object obj, DataType type) 
     {
         string json = JsonConvert.SerializeObject(obj);
+
+        // json 임시 보관 
+        tempChartByjson.Add(type, json);
 
         TypeByChartHandler(type, json);
     }
