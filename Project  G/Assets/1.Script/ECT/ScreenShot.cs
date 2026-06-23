7d39e9041e19aa026fbe127f4e838aa4b5a5a612
김지현 <kjhstar2486@naver.com>
2026-06-23 19:23:15 +0900

    ⚡️ Add: 인게임 스크린샷 임시 코드 추가

---
+++ Project  G/Assets/1.Script/ECT/ScreenShot.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ScreenShot : MonoBehaviour
+{
+    public Camera captureCamera;
+
+    private void Start()
+    {
+        SavePNG();
+    }
+
+    public void SavePNG()
+    {
+
+        int width = 1920;
+        int height = 1080;
+
+        RenderTexture rt = new RenderTexture(width, height, 24);
+        captureCamera.targetTexture = rt;
+
+        Texture2D tex = new Texture2D(width, height, TextureFormat.RGBA32, false);
+
+        captureCamera.Render();
+
+        RenderTexture.active = rt;
+        tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        tex.Apply();
+
+        byte[] bytes = tex.EncodeToPNG();
+        File.WriteAllBytes(Application.dataPath + "/Tilemap.png", bytes);
+
+        captureCamera.targetTexture = null;
+        RenderTexture.active = null;
+
+        DestroyImmediate(rt);
+        DestroyImmediate(tex);
+    }
+}
