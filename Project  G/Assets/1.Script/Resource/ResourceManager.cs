936b6da49e75833282ff7f0859a1c42350fbb46a
김지현 <kjhstar2486@naver.com>
2026-07-31 17:31:15 +0900

    🐛 Fix: 리소스관련 리펙토링 완

--- Project  G/Assets/1.Script/Resource/ResourceManager.cs
+++ Project  G/Assets/1.Script/Resource/ResourceManager.cs
@@ -3,9 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
+public static class ResourceLoaderGeneric 
+{
+    // Load
+    public static T LoadAsset<T>(string path) where T : Object 
+    {
+        var entity = Resources.Load<T>(path);
+        if(entity == null)
+            return null;
+
+        return entity;
+    }
+
+    // LoadeAll
+    public static T[] LoadAssetAll<T>(string path) where T : Object 
+    {
+        var entity = Resources.LoadAll<T>(path);
+        if (entity == null)
+            return null;
+
+        return entity; 
+    }
+}
+
 public class ResourceManager : Singleton<ResourceManager>
 {
-    [SerializeField] private ResourceLoader resourceLoader;
     [SerializeField] private ResourcePath resourcePath;
 
     [Header("===Sprite===")]
@@ -13,29 +35,18 @@ public class ResourceManager : Singleton<ResourceManager>
     [SerializeField] private Sprite[] mapSprite;
     [SerializeField] private Sprite[] characterSprite;
 
-    [Header("===Audio===")]
-    [SerializeField] private AudioMixer mixer;
-    [SerializeField] private AudioClip[] sfxClip;
-    [SerializeField] private AudioClip[] bgmClip;
-
     [Header("===Text Asset===")]
     [SerializeField] private TextAsset fallBackLocalizionTextfile;
     [SerializeField] private TextAsset[] fallBackChartTextfile;
 
     protected override void Singleton_Awake()
     {
-        resourceLoader = new ResourceLoader();
+        defaultSprite = ResourceLoaderGeneric.LoadAsset<Sprite>(resourcePath.GetPathEntity("DefaultSprite"));
+        mapSprite = ResourceLoaderGeneric.LoadAssetAll<Sprite>(resourcePath.GetPathEntity("MapSprite"));
+        characterSprite = ResourceLoaderGeneric.LoadAssetAll<Sprite>(resourcePath.GetPathEntity("CharacterSprite"));
 
-        defaultSprite           = resourceLoader.RoadSprite(resourcePath.DefaultSpritePath + "/" + resourcePath.DefaultSpritePath);
-        mapSprite               = resourceLoader.RoadSpriteAll(resourcePath.MapSpritePath);
-        characterSprite         = resourceLoader.RoadSpriteAll(resourcePath.CharacterSpritePath);
-
-        mixer                   = resourceLoader.RoadMixer(resourcePath.SoundPath + "/" + resourcePath.AudioMixerPath);
-        sfxClip                 = resourceLoader.RoadClipAll(resourcePath.SoundPath + "/" + resourcePath.SfxPath);
-        bgmClip                 = resourceLoader.RoadClipAll(resourcePath.SoundPath + "/" + resourcePath.BgmPath);
-
-        fallBackLocalizionTextfile = resourceLoader.RoadFallBackLocalization(resourcePath.FallBackLocalizationData);
-        fallBackChartTextfile = resourceLoader.RoadeFallBackChartData(resourcePath.FallBackChartData);
+        fallBackLocalizionTextfile = ResourceLoaderGeneric.LoadAsset<TextAsset>(resourcePath.GetPathEntity("FallBackLocalization"));
+        fallBackChartTextfile = ResourceLoaderGeneric.LoadAssetAll<TextAsset>(resourcePath.GetPathEntity("FallBackChart"));
     }
 
     public Sprite GetDefaultSprite() => defaultSprite;
@@ -57,10 +68,6 @@ public class ResourceManager : Singleton<ResourceManager>
 
         return characterSprite[idx];
     }
-
-    public AudioMixer GetAudioMixer => mixer;
-    public AudioClip[] GetSFXClip => sfxClip;
-    public AudioClip[] GetBGMClip => bgmClip;
 
     public TextAsset FallBackLocalizationText => fallBackLocalizionTextfile;
     public TextAsset[] FallBackChartTextfile => fallBackChartTextfile;
--- Project  G/Assets/1.Script/Resource/ResourcePath.cs
+++ Project  G/Assets/1.Script/Resource/ResourcePath.cs
@@ -1,41 +1,64 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.LookDev;
 
-[CreateAssetMenu(fileName = "Resource Data", menuName = "Scriptable Object/Resource Data")]
+public enum ResourceGetMethod 
+{ 
+    Load,
+    LoadAll,
+    None
+}
+
+/// <summary>
+/// - 에디터 단계 
+///     : SerializedProperty를 통해 수정된(입력된) 값(= entities 리스트)이
+///     "ResourcePath"의 entities 필드에 반영되고, .asset 파일에 저장됨
+/// - 빌드 단계
+///     : editor 스크립트는 빌드에 포함되지 X
+///     : "ResourcePath"에서 사용할 때는 .asset에 저장된 정보를 바탕으로 사용 
+/// </summary>
+/// 
+
+[CreateAssetMenu(fileName = "Resource Path", menuName = "Scriptable Object/Resource Data")]
 public class ResourcePath : ScriptableObject
-{
-    [Header("===Sprite===")]
-    [SerializeField] private string defaultSpritePath;
-    public string DefaultSpritePath => defaultSpritePath;
+{    
+    [Serializable]
+    public class PathEntity
+    {
+        public ResourceGetMethod method;
+        public string key;
+        public string value;
+    }
 
-    [SerializeField] private string mapSpritePath;
-    public string MapSpritePath => mapSpritePath;
+    [SerializeField]
+    private List<PathEntity> entities = new List<PathEntity>();
 
-    [SerializeField] private string characterSpritePath;
-    public string CharacterSpritePath => characterSpritePath;
+    // key , 경로객체
+    private Dictionary<string, PathEntity> keyByPathEntity;
 
-    [Space]
-    [Header("===Sound Asset===")]
-    [SerializeField] private string soundPath;
-    public string SoundPath => soundPath;
+    public string GetPathEntity(string key) 
+    {
+        // 초기화 된적이 없으면 
+        // .asset에 저장된 값을 딕셔너리에 채워넣음
+        if (keyByPathEntity == null) 
+        {
+            keyByPathEntity = new Dictionary<string, PathEntity>();
+            foreach (var entity in entities) 
+            {
+                keyByPathEntity.Add(entity.key, entity);
+            }
+        }
 
-    [SerializeField] private string audioMixerPath;
-    public string AudioMixerPath => audioMixerPath; 
-
-    [SerializeField] private string sfxPath;
-    public string SfxPath => sfxPath;
-
-    [SerializeField] private string bgmPath;
-    public string BgmPath => bgmPath;
-
-    [Space]
-    [Header("===Text Asset===")]
-    [SerializeField] private string fallBackLocalizationData;
-    public string FallBackLocalizationData => fallBackLocalizationData;
-
-    [Space]
-    [Header("===Chart Fallback===")]
-    [SerializeField] private string fallBackChartData;
-    public string FallBackChartData => fallBackChartData;
+        if (keyByPathEntity.ContainsKey(key))
+        {
+            return keyByPathEntity[key].value;
+        }
+        else 
+        {
+            Debug.Log($"[Resourcepath] : key {key}를 찾을 수 없습니다.");
+            return null;
+        }
+    }
 }
