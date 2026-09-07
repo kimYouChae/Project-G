7d3dd0abf49b6b2beec1180fa852128b5b7cff45
김지현 <kjhstar2486@naver.com>
2026-09-07 20:07:42 +0900

    👷 Chore: market 맵 테스트코드 삭제

--- Project  G/Assets/1.Script/Data/StageData/StageDataManager.cs
+++ Project  G/Assets/1.Script/Data/StageData/StageDataManager.cs
@@ -122,10 +122,6 @@ public class StageDataManager : Singleton<StageDataManager>
 
     public bool HasMapData(MapType type) 
     {
-// Market 테스트 위한 전처리문
-#if DEV_BUILD_TEST
-    if (type == MapType.Market) return true;
-#endif
         // 맵 타입에 따른 데이터 여부
         // 데이터가 있으면 true, 없으면 false
         return StageDataMaxLength(type) > 0;
